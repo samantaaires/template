f5b4e9b472906762a660d6c294105bcb02766b78
Samanta <samantaferaires@gmail.com>
2017-01-22 22:57:44 -0200

    restful

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({sucess: "get"});
+});
+
+router.put('/', function(req, res, next) {
+  res.json({sucess: "put"});
+});
+
+router.post('/', function(req, res, next) {
+  res.json({sucess: "post"});
+});
+
+router.delete('/', function(req, res, next) {
+  res.json({sucess: "delete"});
 });
 
 module.exports = router;
